3ad169ee05716c61c12c9e555631e37417cad167
rsaifi12 <112189558+rsaifi12@users.noreply.github.com>
2023-10-16 15:52:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import sys
-def handler(event, context):
-    return 'Hello from AWS Lambda using python' + sys.version + '!'
+
+def lambda_handler(event, context):
+    return 'Hello from AWS Lambda using python ' + sys.version + '!'
+
     # return {
     #     'statusCode': 200,
     #     'body': 'Hello, this is a sample Lambda function!'
